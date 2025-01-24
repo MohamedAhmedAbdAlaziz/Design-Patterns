16f5a7f5ee40a30cbf2b974c0bdab1d428f27d6f
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2025-01-24 23:01:20 +0200

    Add Singleton Pattern

---
+++ Design Patterns/A -Creational/01 - Singleton/Singleton/Implementation.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Singleton
+{
+    public class Logger
+    {
+        private static readonly Lazy< Logger> _lazyLooger= new Lazy< Logger>(() => new Logger() );
+        private static Logger? _instance;
+        
+        protected Logger()
+        {
+
+        }
+
+        public static Logger? Instance
+        {
+
+        get {
+                return _lazyLooger.Value;
+                //if(_instance == null)
+                //{
+                //    return new Logger();
+
+                //}
+                //return _instance;
+
+            }
+
+
+        }
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log{ message}");
+        }
+    
+    
+    }
+}
---
+++ Design Patterns/A -Creational/01 - Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using Singleton;
+Console.Title = "Singleton pattern";
+
+var instanse1 = Logger.Instance;
+var instanse2 = Logger.Instance;
+
+if(instanse1 == instanse2 && instanse2== Logger.Instance)
+
+{
+    Console.WriteLine("Instance are the same");
+}
+
+instanse1.Log($"Message from {nameof(instanse1)}");
+instanse1.Log($"Message from {nameof(instanse2)}");
+
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+Console.ReadLine();
